035feeb2992b2f80fcc1d81226f2c42b1bb76587
Fred Siika <fredsiika@gmail.com>
2020-12-06 22:08:46 -0600

    Re-downloaded react-photo-gallery

---
+++ src/components/Gallery/GalleryTitle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const GalleryTitle = () => {
+  return (
+    <div className="text-center">
+      <small>
+        <span className="text-primary lead">Our Work</span>
+      </small>
+      <h4 className="display-3 text-center">Photo Gallery</h4>
+    </div>
+  )
+}
+
--- src/components/Gallery/PhotoGallery.js
+++ src/components/Gallery/PhotoGallery.js
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { render } from 'react-dom'
+import Gallery from 'react-photo-gallery'
 import Carousel, { Modal, ModalGateway } from 'react-images'
+import { photos } from 'components/Gallery/photos'
+import { GalleryTitle } from 'components/Gallery/GalleryTitle'
 
 const PhotoGallery = () => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightBox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
   return (
     <section className="section section-lg pt-lg-0 section-contact-us">
-      <div className="text-center">
-        <small>
-        <span className="text-primary lead">Our Work</span>
-        </small>
-        <h4 className="display-3 text-center">Photo Gallery</h4>
-      </div>
+      <GalleryTitle />
+      <Gallery photos={photos} onClick={openLightBox} />
     </section>
 
   )
